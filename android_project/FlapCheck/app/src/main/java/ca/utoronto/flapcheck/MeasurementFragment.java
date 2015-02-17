6ca33bc1470195aa0040ddbc0356174282be5c51
kmurray <k.murray@utoronto.ca>
2015-02-17 18:54:41 -0500

    Add not implemented Toasts for unimplemented measurements.

--- android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/MeasurementFragment.java
+++ android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/MeasurementFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -28,7 +29,47 @@ public class MeasurementFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_measurement, container, false);
 
-        //Register each of the buttons
+        /*
+         * Register each of the buttons
+         */
+
+        //Temperature
+        Button takeTemp = (Button) view.findViewById(R.id.temperature_button);
+        takeTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Temperature measurement not yet implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Colour
+        Button takeColour = (Button) view.findViewById(R.id.colour_button);
+        takeColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Colour measurement not yet implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Cap Refill
+        Button takeCapRefill = (Button) view.findViewById(R.id.cap_refill_button);
+        takeCapRefill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Capillary refill measurement not yet implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Pulse
+        Button takePulse = (Button) view.findViewById(R.id.pulse_button);
+        takePulse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Pulse measurement not yet implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Photo
         Button takePicture = (Button) view.findViewById(R.id.photo_button);
         takePicture.setOnClickListener(new View.OnClickListener() {
             @Override
