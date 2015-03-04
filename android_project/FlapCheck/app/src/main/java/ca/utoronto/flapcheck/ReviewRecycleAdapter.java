92e13091ce9515ff160d06a376769a0c060ca024
ahwin <ahmadul.hassan@gmail.com>
2015-03-04 13:24:58 -0500

    Dummy temperature summary graph implemented

--- android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/ReviewRecycleAdapter.java
+++ android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/ReviewRecycleAdapter.java
@@ -3,15 +3,21 @@ package ca.utoronto.flapcheck;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 /**
  * Created by ahmadul.hassan on 2015-03-04.
  */
 public class ReviewRecycleAdapter extends RecyclerView.Adapter<ReviewRecycleAdapter.ViewHolder> {
+    private static final String  TAG = "ReviewRecyleAdapter";
     private String[] mDataset;
     private View rootView;
 
@@ -40,15 +46,24 @@ public class ReviewRecycleAdapter extends RecyclerView.Adapter<ReviewRecycleAdap
         View v = null;
 
         switch (viewType) {
+            case R.id.card_review_temp:
+                v = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.card_review_temperature, parent, false);
+                GraphView graph = (GraphView) v.findViewById(R.id.graph_temp_summary);
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                        new DataPoint(0, 1),
+                        new DataPoint(1, 5),
+                        new DataPoint(2, 3),
+                        new DataPoint(3, 2),
+                        new DataPoint(4, 6)
+                });
+                graph.addSeries(series);
+                break;
             case R.id.card_review_colour:
                 v = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.card_review_colour, parent, false);
                 // set the view's size, margins, paddings and layout parameters
                 //        ...
-                break;
-            case R.id.card_review_temp:
-                v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.card_review_temperature, parent, false);
                 break;
             case R.id.card_review_cap_refill:
                 v = LayoutInflater.from(parent.getContext())
@@ -71,10 +86,17 @@ public class ReviewRecycleAdapter extends RecyclerView.Adapter<ReviewRecycleAdap
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
 //        holder.mTextView.setText(mDataset[position]);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onBindViewHolder: itemView type " + holder.getItemViewType());
+                //TODO: implement switch-case statement to launch appropriate review fragment
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -88,7 +110,6 @@ public class ReviewRecycleAdapter extends RecyclerView.Adapter<ReviewRecycleAdap
         //Assume that cards will be displayed in the following order
 //        "TEMPERATURE", "COLOUR", "CAPILLARY REFILL", "PULSE", "PICTURE"
 //              0            1             2              3         4
-
         switch (position) {
             case 0:
                 return R.id.card_review_temp;
