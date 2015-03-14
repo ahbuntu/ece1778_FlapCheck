4106d8968dbb7eb18fe5b773b5821762212142a3
ahwin <ahmadul.hassan@gmail.com>
2015-03-14 19:18:32 -0400

    temperature review page completed

--- android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/ReviewThermaFragment.java
+++ android_project/FlapCheck/app/src/main/java/ca/utoronto/flapcheck/ReviewThermaFragment.java
@@ -3,21 +3,27 @@ package ca.utoronto.flapcheck;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +41,7 @@ import java.util.List;
 public class ReviewThermaFragment extends Fragment {
     private static String TAG = "ReviewThermaFragment";
 
-//    private ReviewTemperatureListAdapter mAdapter;
+    private ReviewTemperatureListAdapter mAdapter;
 
     public interface ReviewThermaFragmentListener {
         Patient getPatient();
@@ -45,6 +51,12 @@ public class ReviewThermaFragment extends Fragment {
 
     public ReviewThermaFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -61,82 +73,101 @@ public class ReviewThermaFragment extends Fragment {
 
         Patient patient = mListenerCallback.getPatient();
         TextView patientHeader = (TextView) view.findViewById(R.id.text_review_therma_patient);
-        patientHeader.setText(patient.getPatientName() +
+        patientHeader.setText(patient.getPatientName() + " " +
                                 "(" + patient.getPatientMRN() + ")");
 
         DBLoaderMeasurement dbLoaderMeasurement = new DBLoaderMeasurement(getActivity());
         List<MeasurementReading> tempReadings = dbLoaderMeasurement.getTemperaturesForPatient(patient.getPatientId());
 
+        ProgressBar spinner = (ProgressBar) view.findViewById(R.id.progress_review_temp);
         GraphView graphTemp = (GraphView) view.findViewById(R.id.graph_review_therma);
         // construct graph here
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+        LineGraphSeries<DataPoint> lineSeries = new LineGraphSeries<DataPoint>();
+        PointsGraphSeries<DataPoint> pointSeries = new PointsGraphSeries<DataPoint>();
         int i = 0;
         for (MeasurementReading mReading : tempReadings) {
             i++;
 //                    Log.d(TAG, "Patient ID: " + mReading.getMeas_patientID() + " Temp: " + mReading.getMeas_temperature());
             //assume that temperature is returned in ascending timestamp order
             DataPoint point = new DataPoint(i, mReading.getMeas_temperature());
-            series.appendData(point, false, tempReadings.size());
+            lineSeries.appendData(point, false, tempReadings.size());
+            pointSeries.appendData(point, false, tempReadings.size());
         }
-        graphTemp.addSeries(series);
+        graphTemp.addSeries(lineSeries);
+        graphTemp.addSeries(pointSeries);
+        graphTemp.setTitle("TEMPERATURE");
+        graphTemp.setTitleColor(getResources().getColor(R.color.fc_dark_gray));
+        graphTemp.setTitleTextSize(24);
         GridLabelRenderer gridStyler =  graphTemp.getGridLabelRenderer();
-        gridStyler.setGridStyle(GridLabelRenderer.GridStyle.NONE);
+        gridStyler.setGridStyle(GridLabelRenderer.GridStyle.BOTH);
+        spinner.setVisibility(View.GONE);
 
-//        mAdapter = new PatientListAdapter(getActivity(),R.layout.patient_list_item, patients);
+        mAdapter = new ReviewTemperatureListAdapter(getActivity(),R.layout.review_therma_list_item, tempReadings);
+        AbsListView mListView = (AbsListView) view.findViewById(R.id.list_review_therma);
+        mListView.setAdapter(mAdapter);
 
         return view;
     }
 
-//    private class ReviewTemperatureListAdapter extends ArrayAdapter<Patient> {
-//        private static final String TAG = "PatientEntryAdapter";
-//        Context mContext;
-//        int layoutResourceId;
-//        List<Patient> patients = null;
-//
-//        /**
-//         * constructor for the adapter
-//         * @param context - context for which it will be displayed
-//         * @param layoutResId - the layout to which the adapter will be bound
-//         * @param list - the list of patients
-//         */
-//        public ReviewTemperatureListAdapter(Context context, int layoutResId, List<Patient> list) {
-//            super(context, layoutResId, list);
-//            mContext = context;
-//            layoutResourceId = layoutResId;
-//            patients = list;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            if(convertView==null){
-//                // inflate the layout
-//                LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
-//                convertView = inflater.inflate(layoutResourceId, parent, false);
-//            }
-//
-//            TextView textViewName  = (TextView) convertView.findViewById(R.id.text_PE_arch_name);
-//            TextView textViewMRN = (TextView) convertView.findViewById(R.id.text_PE_arch_mrn);
-//            TextView textViewOpDateTime  = (TextView) convertView.findViewById(R.id.text_PE_arch_opDateTime);
-//
-//            // object item based on the position
-////        Log.d(TAG, "position of the view to inflate: " + position);
-//            if (patients == null) {
-//                textViewName.setText("");
-//                textViewMRN.setText("");
-//                textViewOpDateTime.setText("");
-//            } else {
-//                Patient item = patients.get(position);
-//                textViewName.setText(item.getPatientName());
-//                textViewMRN.setText(item.getPatientMRN());
-//
-//                String formatStyle = "MMM dd, yyyy";
-//                SimpleDateFormat dateTimeFormat = new SimpleDateFormat(formatStyle);
-//                Calendar cal = new GregorianCalendar();
-//                dateTimeFormat.setTimeZone(cal.getTimeZone());
-////            edit_opDate.setHint(dateTimeFormat.format(cal.getTime()));
-//                textViewOpDateTime.setText(dateTimeFormat.format(cal.getTime()));
-//            }
-//            return convertView;
-//        }
-//    }
+    private class ReviewTemperatureListAdapter extends BaseAdapter {
+        private static final String TAG = "ReviewTempListAdapter";
+        Context mContext;
+        int layoutResourceId;
+        List<MeasurementReading> readingsList = null;
+
+        /**
+         * constructor for the adapter
+         * @param context - context for which it will be displayed
+         * @param layoutResId - the layout to which the adapter will be bound
+         */
+        public ReviewTemperatureListAdapter(Context context, int layoutResId, List<MeasurementReading> readings) {
+//            super(context, layoutResId);
+            mContext = context;
+            layoutResourceId = layoutResId;
+            readingsList = readings;
+        }
+
+        @Override
+        public int getCount () {
+            return readingsList.size();
+        }
+
+        @Override
+        public long getItemId (int position) {
+            return position;
+        }
+
+        @Override
+        public Object getItem (int position) {
+            return readingsList.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                // inflate the layout
+                LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+                convertView = inflater.inflate(layoutResourceId, parent, false);
+            }
+
+            TextView textViewDate = (TextView) convertView.findViewById(R.id.text_review_therma_list_date);
+            TextView textViewTime = (TextView) convertView.findViewById(R.id.text_review_therma_list_time);
+            TextView textViewTemp = (TextView) convertView.findViewById(R.id.text_review_therma_list_temp);
+
+            // object item based on the position
+            Log.d(TAG, "pos to inflate: " + position);
+
+            if (readingsList == null) {
+                textViewDate.setText("");
+                textViewTime.setText("");
+                textViewTemp.setText("");
+            } else {
+                MeasurementReading mReading = readingsList.get(position);
+                textViewDate.setText(Utils.prettyDate(mReading.getMeas_timestamp()));
+                textViewTime.setText(Utils.prettyTime(mReading.getMeas_timestamp()));
+                textViewTemp.setText(Utils.prettyTempCelsius(mReading.getMeas_temperature()));
+            }
+            return convertView;
+        }
+    }
 }
